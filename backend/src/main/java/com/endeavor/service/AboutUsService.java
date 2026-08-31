74df81d0924b979a3349748450729cf1203ca0c4
pogulapavankalyan <pogulavenkatapavan>
2026-08-31 13:57:13 +0530

    Fix import repo in AboutUsService

--- backend/src/main/java/com/endeavor/service/AboutUsService.java
+++ backend/src/main/java/com/endeavor/service/AboutUsService.java
@@ -1,7 +1,7 @@
 package com.endeavor.service;
 
 import com.endeavor.entity.*;
-import com.endeavor.repository.*;
+import com.endeavor.repo.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
